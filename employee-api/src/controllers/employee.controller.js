9d898c137259b198f9f38343116cfdeaa8b242f7
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-05 22:22:42 -0300

    Desenvolvimento da rota 'UpdateEmployeeById'

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -36,3 +36,22 @@ exports.findEmployeeById = async(req, res) => {
   const response = await db.query('SELECT * FROM employee WHERE employee_id = $1', [employeeId]);
   res.status(200).send(response.rows);
 }
+
+// ==> Método responsável por atualizar um determinado 'Employee' por Id:
+exports.updateEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  const { name, job_role, salary, birth, employee_registration } = req.body;
+
+  const response = await db.query(
+    `UPDATE employee 
+      SET name = $1, 
+          job_role = $2, 
+          salary = $3, 
+          birth = $4, 
+          employee_registration = $5 
+      WHERE employee_id = $6`,
+    [name, job_role, salary, birth, employee_registration, employeeId]
+  );
+
+  res.status(200).send({ message: 'Employee Updated Successfully!'});
+}
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -20,4 +20,7 @@ router.get('/employees', employeeController.listAllEmployees);
 // ==> Rota responsável por listar um determinado 'Colaborador' por Id: (GET): localhost:3000/api/employees/:id
 router.get('/employees/:id', employeeController.findEmployeeById)
 
+// ==> Rota reponsável por atualizar um determinado 'Colaborador(a) por Id: (PUT): localhost:3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById)
+
 module.exports = router;
