dc8c6e2b35690232977367aad0ba5c10b16d7761
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-05 22:04:21 -0300

    Desenvolvimento da rota 'ListEmployeeById'

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -29,3 +29,10 @@ exports.listAllEmployees = async(req, res) => {
   const response = await db.query('SELECT * FROM employee ORDER BY name ASC');
   res.status(200).send(response.rows);
 };
+
+// ==> Método responsável por listar um determinado 'Employee' por Id:
+exports.findEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  const response = await db.query('SELECT * FROM employee WHERE employee_id = $1', [employeeId]);
+  res.status(200).send(response.rows);
+}
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -17,4 +17,7 @@ router.post('/employees', employeeController.createEmployee);
 // ==> Rota responsável por listar todos os 'Colaboradores': (GET): localhost:3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
 
+// ==> Rota responsável por listar um determinado 'Colaborador' por Id: (GET): localhost:3000/api/employees/:id
+router.get('/employees/:id', employeeController.findEmployeeById)
+
 module.exports = router;
