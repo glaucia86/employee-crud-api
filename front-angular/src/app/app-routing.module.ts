13e839d886632d05d3e2584396befa71d2189d19
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-14 20:59:33 -0300

    Inclusão de rotas da aplicação no arquivo 'app-routing.module.ts'

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeGetComponent } from './employee-get/employee-get.component';
+
+const routes: Routes = [
+  { path: 'employee/create', component: EmployeeAddComponent },
+  { path: 'employee/:id', component: EmployeeEditComponent },
+  { path: 'employee', component: EmployeeGetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
