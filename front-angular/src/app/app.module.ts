50e8f35bfabbd5e3f8fe5ceb27484164c241c2dd
glaucia86 <glaucia_lemos86@hotmail.com>
2020-09-09 20:12:32 -0300

    Início desenvolvimento do uso do currency pipe.

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import ptBr from '@angular/common/locales/br';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeAddComponent } from './employee-add/employee-add.component';
 import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 import { EmployeeGetComponent } from './employee-get/employee-get.component';
+
+registerLocaleData(ptBr, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -22,7 +27,12 @@ import { EmployeeGetComponent } from './employee-get/employee-get.component';
     SlimLoadingBarModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
