0ca438cbf32bb6655d07cd3160a81556438ade64
glaucia86 <glaucia_lemos86@hotmail.com>
2020-10-05 20:38:12 -0300

    Alteração no arquivo 'App.vue'

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -2,9 +2,8 @@
   <div>
     <!--INÍCIO NAVBAR-->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div class="container">
-        <a class="navbar-brand float-left">CRUD Vue.js + PostgreSQL + Node.js</a>
-          <ul class="nav navbar-nav flex-row float-right">
+        <a class="navbar-brand order-md-last">CRUD Vue.js + PostgreSQL + Node.js + Azure</a>
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item">
               <router-link class="nav-link" to="/">Create New Employee</router-link>
             </li>
@@ -12,7 +11,6 @@
               <router-link class="nav-link" to="/list-employees">List All Employees</router-link>
             </li>
           </ul>
-      </div>
     </nav>
     <!--FIM NAVBAR-->
 
