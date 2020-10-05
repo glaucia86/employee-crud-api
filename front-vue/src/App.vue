5948bf707b2aca9532868c2c59838aae24f792f0
glaucia86 <glaucia_lemos86@hotmail.com>
2020-10-05 20:31:17 -0300

    Pesquenas alterações no arquivo App.vue

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
-    <!--INICIO NAVBAR-->
+    <!--INÍCIO NAVBAR-->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <div class="container">
-        <a class="navbar-brand order-md-last">CRUD Vue.js + PostgreSQL + Node.js</a>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Create New Employee</router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/list-employees">List All Employees</router-link>
-          </li>
-        </ul>
+        <a class="navbar-brand float-left">CRUD Vue.js + PostgreSQL + Node.js</a>
+          <ul class="nav navbar-nav flex-row float-right">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Create New Employee</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/list-employees">List All Employees</router-link>
+            </li>
+          </ul>
       </div>
     </nav>
     <!--FIM NAVBAR-->
+
     <div class="container mt-5">
-      <router-view/>
+      <router-view></router-view>
     </div>
   </div>
 </template>
