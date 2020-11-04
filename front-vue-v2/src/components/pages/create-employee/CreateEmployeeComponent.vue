68c12d9d087a047992a033baea63ff3fe47f8fac
glaucia86 <glaucia_lemos86@hotmail.com>
2020-11-04 20:06:35 -0300

    Adicionando o pacote vuelidate na aplicação

--- front-vue-v2/src/components/pages/create-employee/CreateEmployeeComponent.vue
+++ front-vue-v2/src/components/pages/create-employee/CreateEmployeeComponent.vue
@@ -2,79 +2,73 @@
   <div class="container">
     <div class="card">
       <div class="card-header">
-        <h3>Add New Employee</h3>
+        <h3>Add Employee</h3>
       </div>
       <div class="card-body">
-        <form @susubmit.prevent="createNewEmployee">
-          <!-- INÍCIO BLOCO: Employee Name -->
-          <div class="form-group">
-            <label class="font-weight-bold" for="employee-name">Employee Name</label>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Type your Name"
-              v-model="employee.name"
-            />
-          </div>
-          <!-- FIM BLOCO: Employee Name -->
+      <form v-on:submit.prevent="handleSubmitForm()">
 
-          <!-- INÍCIO BLOCO: Job Role -->
-          <div class="form-group">
-            <label class="font-weight-bold" for="job-role">Job Role</label>
-            <input
-              class="form-control"
-              type="text"
-              placeholder="Type your Job Role"
-              v-model="employee.job_role"
-            />
-          </div>
-          <!-- FIM BLOCO: Job Role -->
+        <!-- INÍCIO BLOCO: Employee Name -->
+        <div class="form-group">
+          <label class="font-weight-bold">Employee Name</label>
+          <input type="text"
+            class="form-control"
+            placeholder="Type Your Name"
+            v-model="employeeForm.name" required
+          >
+        </div>
+        <!-- FIM BLOCO: Employee Name -->
 
-          <!-- INÍCIO BLOCO: Salary -->
-          <div class="form-group">
-            <label class="font-weight-bold" for="salary">Salary</label>
-            <input
-              class="form-control"
-              type="number"
-              placeholder="Type your Salary"
-              v-model="employee.salary"
-            />
-          </div>
-          <!-- FIM BLOCO: Salary -->
+        <!-- INÍCIO BLOCO: Job Role -->
+        <div class="form-group">
+          <label class="font-weight-bold">Job Role</label>
+          <input type="text"
+            class="form-control"
+            placeholder="Type Your Job Role"
+            v-model="employeeForm.job_role"
+            required
+          >
+        </div>
+        <!-- FIM BLOCO: Job Role -->
 
-          <!-- INÍCIO BLOCO: Birth -->
-          <div class="form-group">
-            <label class="font-weight-bold" for="birth">Birth</label>
-            <input
-              class="form-control"
-              type="date"
-              placeholder="YYYY/MM/DD/"
-              v-model="employee.birth"
-            />
-          </div>
-          <!-- FIM BLOCO: Birth -->
+        <!-- INÍCIO BLOCO: Salary -->
+        <div class="form-group">
+          <label class="font-weight-bold">Salary</label>
+          <input type="number"
+            class="form-control"
+            placeholder="Type Your Salary"
+            v-model="employeeForm.salary"
+            required
+          >
+        </div>
+        <!-- FIM BLOCO: Salary -->
 
-          <!-- INÍCIO BLOCO: Employee Registration -->
-          <div class="form-group">
-            <label
-              class="font-weight-bold"
-              for="employee-registration">Employee Registration
-            </label>
-            <input
-              class="form-control"
-              type="text"
-              placeholder="Type your Employee Registration"
-              v-model="employee.employee_registration"
-            />
-          </div>
-          <!-- FIM BLOCO: Employee Registration -->
-          <div class="form-group">
-            <button
-              type="submit"
-              class="btn btn-primary">+ Employee
-            </button>
-          </div>
-        </form>
+        <!-- INÍCIO BLOCO: Birth -->
+        <div class="form-group">
+          <label class="font-weight-bold">Birth</label>
+          <input type="date"
+            class="form-control"
+            placeholder="YYYY/MM/DD"
+            v-model="employeeForm.birth"
+            required
+          >
+        </div>
+        <!-- FIM BLOCO: Birth -->
+
+        <!-- INÍCIO BLOCO: Employee Registration -->
+        <div class="form-group">
+          <label class="font-weight-bold">Employee Registration</label>
+          <input type="text"
+            class="form-control"
+            placeholder="Employee Registration"
+            v-model="employeeForm.employee_registration"
+            required
+          >
+        </div>
+        <!-- FIM BLOCO: Employee Registration -->
+        <div class="form-group">
+          <button class="btn btn-primary" type="submit">+ Employee</button>
+        </div>
+      </form>
       </div>
     </div>
   </div>
@@ -85,7 +79,7 @@ export default {
   name: 'CreateEmployeeComponent',
   data() {
     return {
-      employee: {
+      employeeForm: {
         name: '',
         job_role: '',
         salary: '',
@@ -95,9 +89,7 @@ export default {
     };
   },
   methods: {
-    handleSubmitForm() {
-      // TODO - Implementar a lógica do botão!
-    },
+    handleSubmitForm() {},
   },
 };
 </script>
--- front-vue-v2/src/main.js
+++ front-vue-v2/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
+import Vuelidate from 'vuelidate';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/nprogress/nprogress.css';
@@ -9,6 +10,7 @@ import '../node_modules/nprogress/nprogress.css';
 Vue.config.productionTip = false;
 
 Vue.use(VueSweetalert2);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
