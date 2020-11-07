ba3a463b71d5afe9aeee352e9fa9b233e8292a0d
glaucia86 <glaucia_lemos86@hotmail.com>
2020-11-07 19:28:12 -0300

    Desenvolvimento do arquivo 'EmployeeService.js'

--- front-vue-v2/src/services/EmployeeService.js
+++ front-vue-v2/src/services/EmployeeService.js
@@ -5,3 +5,71 @@
  * author: Glaucia Lemos <twitter: @glaucia_lemos86>
  */
 
+import Api from './Api';
+
+export default {
+  /**
+   * Método responsável por criar um novo(a) 'Employee'
+   * (POST): localhost:3000/api/employees
+   */
+  async createNewEmployee(employee) {
+    try {
+      const response = await Api().post('/employees', employee);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  /**
+   * Método responsável por listar todos os 'Employees'
+   * (GET): localhost:3000/api/employees
+   */
+  async getEmployees() {
+    try {
+      const response = await Api().get('/employees');
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  /**
+   * Método responsável por Listar por Id um determinado 'Employee'
+   * (GET): localhost:3000/api/employees/:id
+   */
+  async getEmployeeId(id) {
+    try {
+      const response = await Api().get(`/employees/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  /**
+   * Método responsável por atualizar um determinado 'Employee' por Id
+   * (PUT): localhost:3000/api/employees/:id
+   */
+  async updateEmployee(id) {
+    try {
+      const response = await Api().put(`/employees/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  /**
+   * Método responsável por excluir um determinado 'Employee' por Id
+   * (DELETE): localhost:3000/api/employees/:id
+   */
+  async deleteEmployee(id) {
+    try {
+      const response = await Api().delete(`/employees/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
