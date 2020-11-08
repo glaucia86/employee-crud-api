4a35acd8930ca2227eee25815861284c91a142e7
glaucia86 <glaucia_lemos86@hotmail.com>
2020-11-08 19:10:29 -0300

    Desenvolvimento do arquivo Api.js

---
+++ front-vue-v2/src/services/Api.js
@@ -0,0 +1,13 @@
+/**
+ * file: src/services/Api.js
+ * data: 09/11/2020
+ * description: arquivo responsável por inicializar o 'axios' e as requisições da url base dos HTTP's
+ * author: Glaucia Lemos <twitter: @glaucia_lemos86>
+ */
+
+import axios from 'axios';
+
+export default () => axios.create({
+  // 'baseURL' do Back-End -> fará a comunicação do Front com o Back
+  baseURL: 'http://localhost:3000/api',
+});
---
+++ front-vue-v2/src/services/EmployeeService.js
@@ -0,0 +1,7 @@
+/**
+ * file: src/services/EmployeeService.js
+ * data: 09/11/2020
+ * description: arquivo responsável pelos métodos de requisições das Apís via HTTP
+ * author: Glaucia Lemos <twitter: @glaucia_lemos86>
+ */
+
