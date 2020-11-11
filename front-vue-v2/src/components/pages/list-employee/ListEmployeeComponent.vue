3f4cfaff7531686687c24ffa5db504da64022722
glaucia86 <glaucia_lemos86@hotmail.com>
2020-11-11 20:32:56 -0300

    Desenvolvimento do template da página 'ListEmployeeComponent.vue'

--- front-vue-v2/src/components/pages/list-employee/ListEmployeeComponent.vue
+++ front-vue-v2/src/components/pages/list-employee/ListEmployeeComponent.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="row justify-content-center">
-    <h1>List All Employees</h1>
+  <div>
+    <h3 class="text-center">List All Employees</h3>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th class="text-center">Employee Name</th>
+          <th class="text-center">Job Role</th>
+          <th class="text-center">Salary</th>
+          <th class="text-center">Birth</th>
+          <th class="text-center">Employee Registration</th>
+          <th class="text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee._id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.job_role }}</td>
+          <td>{{ employee.salary }}</td>
+          <td>{{ employee.birth }}</td>
+          <td>{{ employee.employee_registration }}</td>
+          <td>
+            <router-link
+              :to="{ name: 'update', params: { id: employee._id } }"
+              class="btn btn-success">
+            </router-link>
+              <font-awesome-icon :icon="['fas', 'user-edit']" /> Edit
+          </td>
+          <td>
+            <button class="btn btn-danger">
+              <font-awesome-icon :icon="['fas', 'trash']" /> Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,7 +42,7 @@ export default {
   name: 'ListEmployeeComponent',
   data() {
     return {
-
+      employees: [],
     };
   },
 };
