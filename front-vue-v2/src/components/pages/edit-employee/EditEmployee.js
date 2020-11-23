393c1fdceace4b6f1322a673f0ad798cfef4d07c
glaucia86 <glaucia_lemos86@hotmail.com>
2020-11-23 20:49:15 -0300

    Desenvolvimento do componente 'EditEmployee.vue'

---
+++ front-vue-v2/src/components/pages/edit-employee/EditEmployee.js
@@ -0,0 +1,16 @@
+/**
+ * file: src/components/pages/edit-employee/EditEmployee.js
+ * data: 23/11/2020
+ * description: arquivo responsável pela lógica do componente
+ *  'EditEmployeeComponent.vue'
+ * author: Glaucia Lemos <twitter: @glaucia_lemos86>
+ */
+
+export default {
+  name: 'EditEmployeeComponent',
+  data() {
+    return {
+      employeeForm: {},
+    };
+  },
+};
--- front-vue-v2/src/components/pages/edit-employee/EditEmployeeComponent.vue
+++ front-vue-v2/src/components/pages/edit-employee/EditEmployeeComponent.vue
@@ -1,16 +1,87 @@
 <template>
-  <div class="row justify-content-center">
-    <h1>Update Employee</h1>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Update Employee</h3>
+      </div>
+      <div class="card-body">
+        <form v-on:submit.prevent="updateFormEmployee()">
+
+          <!--INÍCIO DO BLOCO: Employee Name-->
+          <div class="form-group">
+            <label class="font-weight-bold">Employee Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              class="form-control"
+              v-model="employeeForm.name"
+            >
+          </div>
+          <!--FIM DO BLOCO: Employee Name-->
+
+          <!--INÍCIO DO BLOCO: Job Role-->
+          <div class="form-group">
+            <label class="font-weight-bold">Job Role</label>
+            <input
+              type="text"
+              id="job_role"
+              name="job_role"
+              class="form-control"
+              v-model="employeeForm.job_role"
+            >
+          </div>
+          <!--FIM DO BLOCO: Job Role-->
+
+          <!--INÍCIO DO BLOCO: Salary-->
+          <div class="form-group">
+            <label class="font-weight-bold">Salary</label>
+            <input
+              type="number"
+              id="salary"
+              name="salary"
+              class="form-control"
+              v-model="employeeForm.salary"
+            >
+          </div>
+          <!--FIM DO BLOCO: Job Role-->
+
+          <!--INÍCIO DO BLOCO: Birth-->
+          <div class="form-group">
+            <label class="font-weight-bold">Birth</label>
+            <input
+              type="date"
+              id="birth"
+              name="birth"
+              class="form-control"
+              v-model="employeeForm.salary"
+            >
+          </div>
+          <!--FIM DO BLOCO: Birth-->
+
+          <!--INÍCIO DO BLOCO: Employee Registration-->
+          <div class="form-group">
+            <label class="font-weight-bold">Employee Registration</label>
+            <input
+              type="text"
+              id="employee_registration"
+              name="employee_registration"
+              class="form-control"
+              v-model="employeeForm.employee_registration"
+            >
+          </div>
+          <!--FIM DO BLOCO: Birth-->
+          <div class="form-group">
+            <button @click="submit" class="btn btn-primary">
+              <font-awesome-icon :icon="['fas', 'user-edit']"/> Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'EditEmployeeComponent',
-  data() {
-    return {
+<script src="./EditEmployee.js"></script>
 
-    };
-  },
-};
-</script>
+<style src="" scoped></style>
