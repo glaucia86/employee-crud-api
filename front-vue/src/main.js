f64a773782911e1d23c5da4c4651163081778e92
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-25 18:14:43 -0300

    Atualização do repositório - remoção de projeto desnecessário.

--- front-vue/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-createApp(App).use(router).mount('#app');
